89020a8b817bac42aa55b1ce336bf8d9999f9804
MUJAWAMARIYAbundle <iradupeteryves@gmail.com>
2019-11-26 22:14:54 +0200

    add active dmin and devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,9 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local = false
-  #letter  opener
-  config.action_mailer.delivery_method = :letter_opener
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
