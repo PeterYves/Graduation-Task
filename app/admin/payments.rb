439e61685e839dea9b246565765c5747eee7da83
MUJAWAMARIYAbundle <iradupeteryves@gmail.com>
2019-12-01 06:12:21 +0200

    design devise views and forms

--- app/admin/payments.rb
+++
@@ -1,9 +0,0 @@
-ActiveAdmin.register Payment do
-  before_filter do
-    Payment.class_eval do
-      def to_param
-        id.to_s
-      end
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   resources :courses do
     resources :comments
   end
@@ -10,5 +8,6 @@ Rails.application.routes.draw do
   resources :pages
   root to: 'pages#home'
   devise_for :users, :controllers => { registrations: 'registrations' }
-
+  resources :users
+  
 end
