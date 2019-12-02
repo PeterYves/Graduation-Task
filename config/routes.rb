067a00c6990a0eb45bb2bbb3b0082afce3598c89
MUJAWAMARIYAbundle <iradupeteryves@gmail.com>
2019-12-02 17:54:12 +0200

    finalising design and adding controls in contrllers

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :charges
   resources :payments
   resources :pages
-  root to: 'pages#home'
+  root to: 'courses#index'
   devise_for :users, :controllers => { registrations: 'registrations'}
   resources :users
   
