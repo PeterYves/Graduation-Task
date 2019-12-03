ae0f4ed4dd4d1db4f0ec2197f0b24600fe8d44ac
MUJAWAMARIYAbundle <iradupeteryves@gmail.com>
2019-12-03 18:38:04 +0200

    erasing comments in course controller

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,9 +1,9 @@
 class Payment < ApplicationRecord
-#   after_create :email_payment
+  after_create :email_payment
   def to_param
     uuid
   end
-#   def email_payment
-#     PaymentMailer.payment_receipt(self) .deliver
-#   end
+  def email_payment
+    PaymentMailer.payment_receipt(self) .deliver
+  end
 end
