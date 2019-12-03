760cacebeb8079f358bd57f80e4fcae6a311afcf
MUJAWAMARIYAbundle <iradupeteryves@gmail.com>
2019-12-03 17:19:17 +0200

    correcting some css errors

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,10 +1,10 @@
 class Payment < ApplicationRecord
-    # after_create :email_payment
-    def to_param
-        uuid
-    end
+  after_create :email_payment
+  def to_param
+    uuid
+  end
 
-    # def email_payment
-    #     PaymentMailer.payment_receipt(self) .deliver
-    # end
+  def email_payment
+    PaymentMailer.payment_receipt(self) .deliver
+  end
 end
