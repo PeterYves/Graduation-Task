1c05995315a072ec1f76aa04ff1f119c54723ba1
MUJAWAMARIYAbundle <iradupeteryves@gmail.com>
2019-12-03 10:59:35 +0200

    adding seed and error pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: 'Petro', email: 'petro@gmail.com', password: '123456', password_confirmation: '123456', user_type:'admin')
+User.create(names: 'Petro', email: 'petro@gmail.com', password: '123456', password_confirmation: '123456', user_type:'admin')
 User.create(names: 'mucyo', email: 'mucyo@gmail.com', password: '123456', password_confirmation: '123456',user_type:'admin' )
 User.create(names: 'muhire', email: 'muhire@gmail.com', password: '123456', password_confirmation: '123456')
 User.create(names: 'emma', email: 'emma@gmail.com', password: '123456', password_confirmation: '123456', user_type:'admin' )
@@ -17,4 +17,4 @@ User.create(names: 'kiko casilla', email: 'kiko@gmail.com', password: '123456', 
 User.create(names: 'amina', email: 'amina@gmail.com', password: '123456', password_confirmation: '123456' )
 
 
-Course.create(name: 'javascript', description: 'Master JavaScript With The Most Complete Course On The Market. From Beginner to Advanced.', price: 300, clip: 'to night jayc social mula.mp4',thumbnail:'Pia Pounds - Easy (Official HD Video) _ New Ugandan Music Videos _ DjZeRoPrO - YouTube (1080p).mp4')
+
